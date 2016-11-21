5952f0d85948cc0b9efd10a51aae2299ea56d61d
Jaismeen Sandhu <jaismeen.sandhu@yahoo.com>
2016-11-21 01:21:19 -0500

    make it better

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var pg = require('pg');
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM cities', function(err, result) {
+    client.query('SELECT * FROM city', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
