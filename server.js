d28e5e21978cc4a077b689fe0f5d5d509ed8697f
Jaismeen Sandhu <jaismeen.sandhu@yahoo.com>
2016-11-21 03:28:36 -0500

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-//var mongodb = require("mongodb");
-var mongodb = require('pg');
+var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
@@ -14,13 +13,8 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-//const conString = 'postgres://Jaismeen:admin@process.env.DATABASE_URL' // make sure to match your own database's credentials
-
-//DATABASE_URL='postgres://Jaismeen:admin@localhost:5000'
-
-// Connect to the database before starting the application server. 
-//mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-mongodb.connect(process.env.DATABASE_URL, function (err, database) {
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -31,7 +25,7 @@ mongodb.connect(process.env.DATABASE_URL, function (err, database) {
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 5000, function () {
+  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
@@ -55,7 +49,7 @@ app.get("/contacts", function(req, res) {
     if (err) {
       handleError(res, err.message, "Failed to get contacts.");
     } else {
-      res.status(200).json(docs);  
+      res.status(200).json(docs);
     }
   });
 });
@@ -88,7 +82,7 @@ app.get("/contacts/:id", function(req, res) {
     if (err) {
       handleError(res, err.message, "Failed to get contact");
     } else {
-      res.status(200).json(doc);  
+      res.status(200).json(doc);
     }
   });
 });
