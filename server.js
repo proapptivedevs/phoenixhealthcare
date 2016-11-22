c6ef4c49907d4fcc26f5d0e78159814c0c30b81f
Jaismeen Sandhu <jaismeen.sandhu@yahoo.com>
2016-11-21 21:47:07 -0500

    Database

--- server.js
+++ server.js
@@ -4,19 +4,27 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var PATIENTS_COLLECTION = "patients";
-var TESTS_COLLECTION = "tests";
-var RECORDS_COLLECTION = "records";
+var PATIENTS = "patients";
+var TESTS = "tests";
+var RECORDS = "records";
+var USERS = "users";
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+//const IP = process.env.MONGODB_IP_ADDRESS | '127.0.0.1',
+//PORT = process.env.MONGODB_PORT | 27017;
+
 var db;
 
+var URI = process.env.MONGODB_URI;
+if (!URI)
+  URI = 'mongodb://127.0.0.1:27017/eHealthCare';
+
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -25,37 +33,10 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
-  var samplePatientpost = {
-    "name": "Jill Collins",
-    "age": 26,
-    "gender": "F",
-    "bloodType": "B",
-    "testData": {
-    			"bloodPressure": 39,
-    			"respiratoryRate": 64,
-    			"bloodOxygenlevel": "high",
-    			"heartBeatRate":9
-    			},
-    "records": {
-    			"bloodPressure": 29,
-    			"respiratoryRate": 93,
-    			"bloodOxygenlevel": "fair",
-    			"heartBeatRate":11
-    			}			
-  };
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
+  var server = app.listen(process.env.PORT || 5000, function () {
     var port = server.address().port;
     console.log("App is running at port", port);
-   /* console.log('Server %s listening at %s', server.name)
-    console.log('Resources:')
-    console.log('/patients')
-    console.log('Sample Patient post Data: ')
-    console.log(samplePatientpost)
-    console.log('Sample Patient Get Data: ')
-    console.log('/patients/:id')
-    console.log('Get all Patients : ')
-    console.log('/patients')*/
   });
 });
 
@@ -73,7 +54,7 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/patients", function(req, res) {
-  db.collection(PATIENTS_COLLECTION).find({}).toArray(function(err, docs) {
+  db.collection(PATIENTS).find({}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get patients.");
     } else {
@@ -90,7 +71,7 @@ app.post("/patients", function(req, res) {
     handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
   }
 
-  db.collection(PATIENTS_COLLECTION).insertOne(newPatient, function(err, doc) {
+  db.collection(PATIENTS).insertOne(newPatient, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to create new patient.");
     } else {
@@ -106,9 +87,9 @@ app.post("/patients", function(req, res) {
  */
 
 app.get("/patients/:id", function(req, res) {
-  db.collection(PATIENTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+  db.collection(PATIENTS).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get patient");
+      handleError(res, err.messsage, "Failed to get patient");
     } else {
       res.status(200).json(doc);
     }
@@ -119,7 +100,7 @@ app.put("/patients/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(PATIENTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(PATIENTS).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update patient");
     } else {
@@ -129,7 +110,7 @@ app.put("/patients/:id", function(req, res) {
 });
 
 app.delete("/patients/:id", function(req, res) {
-  db.collection(PATIENTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+  db.collection(PATIENTS).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
       handleError(res, err.message, "Failed to delete patient");
     } else {
